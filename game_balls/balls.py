e6327893dd89df6cb7f12016e46e5fcadbf6a61e
Zaytsev Sergei <bonifacyi1102@gmail.com>
2020-02-04 14:25:40 +0300

    Template for balls

--- game_balls/balls.py
+++ game_balls/balls.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
 
 import tkinter as tk
+from random import randint
+
+WIDTH = 500
+HEIGHT = 400
 
 
 def canvas_click_handler(event):
     print('Coordinates: x=', event.x, 'y=', event.y)
 
 
+def tick():
+    global x, y, dx, dy
+    x += dx
+    y += dy
+    if x + R > WIDTH or x - R <= 0:
+        dx = -dx
+    if y + R > HEIGHT or y - R <= 0:
+        dy = -dy
+
+    canvas.move(ball_id, dx, dy)
+    root.after(5, tick)
+
+
 def main():
     global root, canvas
-    global x, y, z
+    global ball_id, x, y, dx, dy, R
     root = tk.Tk()
-    canvas = tk.Canvas(root)
+    root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+    canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
     canvas.pack()
     canvas.bind('<Button-1>', canvas_click_handler)
 
+    R = randint(20, 50)
+    x = randint(R, WIDTH - R)
+    y = randint(R, HEIGHT - R)
+    dx, dy = (+1, +1)
+    ball_id = canvas.create_oval(x - R, y - R, x + R, y + R, fill='green')
 
+    tick()
 
     root.mainloop()
 
