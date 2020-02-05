a99867719aa7739af4e310267ea4f95a64acd976
Zaytsev Sergei <bonifacyi1102@gmail.com>
2020-02-05 12:13:32 +0300

    Balls collision

--- game_balls/balls.py
+++ game_balls/balls.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
 import tkinter as tk
-from random import randint
+from random import randint, choice
 
-WIDTH = 500
-HEIGHT = 400
+WIDTH = 1000
+HEIGHT = 800
+COLOR = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
 
 
 class Ball:
-    def __init__(self):
+    def __init__(self, canvas, x, y):
+        self.canvas = canvas
         self.R = randint(20, 50)
-        self.x = randint(self.R, WIDTH - self.R)
-        self.y = randint(self.R, HEIGHT - self.R)
-        self.dx, self.dy = (+2, +3)
-        self.ball_id = canvas.create_oval(self.x - self.R, self.y - self.R,
-                                          self.x + self.R, self.y + self.R, fill='green')
+        self.x = x
+        self.y = y
+        self.dx = randint(-3, 3)
+        self.dy = randint(-3, 3)
+        self.color = choice(COLOR)
+        self.ball_id = self.create_ball()
+        self.level = 50 * (self.dx ** 2 + self.dy ** 2) // self.R
+
+    def create_ball(self):
+        if self.x < self.R:
+            x = self.R
+        elif self.x > WIDTH - self.R:
+            x = WIDTH - self.R
+        else:
+            x = self.x
+        if self.y < self.R:
+            y = self.R
+        elif self.y > HEIGHT - self.R:
+            y = HEIGHT - self.R
+        else:
+            y = self.y
+
+        return self.canvas.create_oval(x - self.R, y - self.R, x + self.R, y + self.R, fill=self.color)
 
     def move(self):
         self.x += self.dx
@@ -25,37 +45,53 @@ class Ball:
             self.dy = -self.dy
 
     def show(self):
-        canvas.move(self.ball_id, self.dx, self.dy)
+        self.canvas.move(self.ball_id, self.dx, self.dy)
 
-    def check_inside(self):
+    def check_inside(self, balls):
+        for ball in balls:
+            delta = ((self.x - ball.x) ** 2 + (self.y - ball.y) ** 2) ** 0.5
+            if delta < self.R + ball.R and self != ball:
+                self.dx, ball.dx = ball.dx, self.dx     # TODO
+                self.dy, ball.dy = ball.dy, self.dy
+
+    def check_collision(self, balls):
         pass
 
-    def check_collision(self):
-        pass
+    def check_destroy(self, check_x, check_y):
+        if (check_x - self.x) ** 2 + (check_y - self.y) ** 2 <= self.R ** 2:
+            self.canvas.delete(self.ball_id)
+            return True
+        else:
+            return False
 
 
-def canvas_click_handler(event):
-    print('Coordinates: x=', event.x, 'y=', event.y)
+class Block:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+        self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+        self.canvas.bind('<Button-1>', self.canvas_click_handler)
+        self.balls = []
+        self.tick()
 
+    def canvas_click_handler(self, event):
+        for ball in self.balls:
+            if ball.check_destroy(event.x, event.y):
+                return
+        self.balls.append(Ball(self.canvas, event.x, event.y))
 
-def tick():
-    ball.move()
-    ball.show()
-    root.after(5, tick)
+    def tick(self):
+        for ball in self.balls:
+            ball.move()
+            ball.check_inside(self.balls)
+            ball.show()
+        self.root.after(10, self.tick)
 
 
 def main():
-    global root, canvas, ball
-
-    root = tk.Tk()
-    root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
-    canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
-    canvas.pack()
-    canvas.bind('<Button-1>', canvas_click_handler)
-    ball = Ball()
-    tick()
-
-    root.mainloop()
+    Block()
+    tk.mainloop()
 
 
 if __name__ == '__main__':
